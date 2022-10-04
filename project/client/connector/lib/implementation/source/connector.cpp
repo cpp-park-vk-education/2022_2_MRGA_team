c1e4425c31cca6a3b0a9adf3fa833e54b9e926a3
Khasbulat Abdullin <khas.2015@gmail.com>
2022-10-04 19:34:59 +0300

    улучшил cmake файлы

--- project/client/connector/lib/implementation/source/connector.cpp
+++ project/client/connector/lib/implementation/source/connector.cpp
@@ -4,6 +4,10 @@ using connector = http_request_connector;
 
 Response<bool> connector::authorization(const string &login,
                                         const string &password) {
+    http::Request request{"http://localhost/test"};
+
+    // send a get request
+    const auto response = request.send("GET");
     return {true, error_code::success};
 }
 
