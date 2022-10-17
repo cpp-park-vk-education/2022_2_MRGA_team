82f7ea5971875947480655311fdf082a89691fad
Zela2520 <sugarbeatzela@gmail.com>
2022-10-17 10:56:37 +0300

    fixed bags in new branch

--- project/client/interface/build-PartyTime-Desktop_x86_darwin_generic_mach_o_64bit-Debug/API/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sugarbearzelagmail.com/Documents/IT/cplusplusproject/project/client/interface/PartyTime/API
-# Build directory: /Users/sugarbearzelagmail.com/Documents/IT/cplusplusproject/project/client/interface/build-PartyTime-Desktop_x86_darwin_generic_mach_o_64bit-Debug/API
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- project/client/interface/build-PartyTime-Desktop_x86_darwin_generic_mach_o_64bit-Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sugarbearzelagmail.com/Documents/IT/cplusplusproject/project/client/interface/PartyTime
-# Build directory: /Users/sugarbearzelagmail.com/Documents/IT/cplusplusproject/project/client/interface/build-PartyTime-Desktop_x86_darwin_generic_mach_o_64bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Components")
-subdirs("Pages")
-subdirs("API")
