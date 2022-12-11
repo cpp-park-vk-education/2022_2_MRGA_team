e9e919e9a62b1ec340dc347f2d3d468205b70ec5
Khasbulat Abdullin <khas.2015@gmail.com>
2022-12-11 05:16:06 +0300

    deleted files

--- project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/d/72bf2e856d6e122bdce17cd1c0f14090e7a20350552340264b257084eb739a35.d
+++
@@ -1,8 +0,0 @@
-PartyTime/connector/lib/PartyTimeConnector/PartyTimeConnector_autogen/timestamp: \
-  /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/PartyTime/connector/lib/PartyTimeConnector/CMakeLists.txt \
-  /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/PartyTime/connector/lib/PartyTimeConnector/implementation/src/AuthConnector.cpp \
-  /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/PartyTime/connector/lib/PartyTimeConnector/implementation/src/EventsConnector.cpp \
-  /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/PartyTime/connector/lib/PartyTimeConnector/implementation/src/PartyTimeConnector.cpp \
-  /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/PartyTime/connector/lib/PartyTimeConnector/implementation/src/UsersConnector.cpp \
-  /Users/sugarbearzelagmail.com/Qt/Qt\ Creator.app/Contents/Resources/package-manager/auto-setup.cmake \
-  /Users/sugarbearzelagmail.com/Qt/Tools/CMake/CMake.app/Contents/bin/cmake
--- project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/interface/PartyTime/Components/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/interface/PartyTime/Components
-# Build directory: /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/interface/PartyTime/Components
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("UI")
