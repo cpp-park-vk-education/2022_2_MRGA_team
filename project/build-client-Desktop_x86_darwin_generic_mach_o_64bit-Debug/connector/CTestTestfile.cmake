cc101811513ff428ad67456d2eab69856cf92658
Zela2520 <sugarbeatzela@gmail.com>
2022-12-16 20:39:43 +0300

    removed redundant files

--- project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/connector/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/connector
-# Build directory: /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/connector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("tests")
