104f714a93a29da5f7d36921a50a491e1102bb9d
Khasbulat Abdullin <khas.2015@gmail.com>
2022-12-17 23:16:02 +0300

    удаление лишних файлов

--- project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/PartyTime/connector/lib/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/client/PartyTime/connector/lib
-# Build directory: /Users/sugarbearzelagmail.com/Documents/IT/TechPark/2022_2_MRGA_team/project/build-client-Desktop_x86_darwin_generic_mach_o_64bit-Debug/PartyTime/connector/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("HttpConnector")
-subdirs("LocalStorage")
-subdirs("PartyTimeConnector")
