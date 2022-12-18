26fd9484be78d563269547afe7c365301d27f2d2
bodja_n <bs@mt11.su>
2022-12-18 13:56:42 +0300

    tmp save

---
+++ local_build/project/server/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bodja_n/dev/c++/2022_2_MRGA_team/project/server/main.cpp" "project/server/CMakeFiles/server.dir/main.cpp.o" "gcc" "project/server/CMakeFiles/server.dir/main.cpp.o.d"
+  "/home/bodja_n/dev/c++/2022_2_MRGA_team/project/server/src/http_connection.cpp" "project/server/CMakeFiles/server.dir/src/http_connection.cpp.o" "gcc" "project/server/CMakeFiles/server.dir/src/http_connection.cpp.o.d"
+  "/home/bodja_n/dev/c++/2022_2_MRGA_team/project/server/src/tcp_server.cpp" "project/server/CMakeFiles/server.dir/src/tcp_server.cpp.o" "gcc" "project/server/CMakeFiles/server.dir/src/tcp_server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bodja_n/dev/c++/2022_2_MRGA_team/local_build/project/server/handler/CMakeFiles/handler_lib.dir/DependInfo.cmake"
+  "/home/bodja_n/dev/c++/2022_2_MRGA_team/local_build/project/server/handler/service/CMakeFiles/service_lib.dir/DependInfo.cmake"
+  "/home/bodja_n/dev/c++/2022_2_MRGA_team/local_build/project/server/handler/service/repository/lib/CMakeFiles/postgres_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ local_build/project/server/handler/service/CMakeFiles/service_lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for service_lib.
+# This may be replaced when dependencies are built.
