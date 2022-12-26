04318817b19dfd2b693f480f3560c405b3f92fca
Khasbulat Abdullin <khas.2015@gmail.com>
2022-12-26 04:44:25 +0300

    убрал флаги тестирования, тк мешает сборке

--- project/client/PartyTime/connector/cmake/flags.cmake
+++ project/client/PartyTime/connector/cmake/flags.cmake
@@ -6,4 +6,4 @@ set(cxx_flags "-Werror -Wall -Wextra -Wpedantic\
 set(sanitizers_flags "-fsanitize=address -fsanitize-undefined-trap-on-error -fno-sanitize-recover=all -g -O0 "
         CACHE STRING sanitizers_flags FORCE)
 
-set(testing_flags "-fprofile-arcs -ftest-coverage -fPIC -O0 -coverage" CACHE STRING testing_flags FORCE)
+# set(testing_flags "-fprofile-arcs -ftest-coverage -fPIC -O0 -coverage" CACHE STRING testing_flags FORCE)
